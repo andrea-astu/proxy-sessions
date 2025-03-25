cf78b2b460b6a1c0ba0706612c4e20dad56b3454
andrea-astu <162191015+andrea-astu@users.noreply.github.com>
2025-03-25 14:40:37 -0600

    adjusted server payload valaue

--- geekpython_example_server.py
+++ geekpython_example_server.py
@@ -41,7 +41,7 @@ async def ws_server(websocket):
                         a = json.loads(await websocket.recv()) # receive number
                         b = json.loads(await websocket.recv()) # receive number
                         c = a + b
-                        await websocket.send(json.dumps("c")) # convert payload to json and send to proxy
+                        await websocket.send(json.dumps(c)) # convert payload to json and send to proxy
                         print(f'Sent payload: {c}')
                         protocol == "A"
                     
