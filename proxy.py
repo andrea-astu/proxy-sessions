fb3f02bf23b71e06e2ed6294f9a1422704a96973
andrea-astu <162191015+andrea-astu@users.noreply.github.com>
2025-05-05 14:08:01 +0200

    moved session types to a new file

--- proxy.py
+++ proxy.py
@@ -15,136 +15,14 @@ import json
 # to define ports as flags (optional arguments)
 import argparse
 
-# ------------------- define session types -----------------------------------------------
+#
+from session_types import * # for session types, dictionary and schema error
 
-# define dir and label
-class Dir:
-    def __init__(self, dir: str):
-        self.dir = dir # only send or recv
-
-class Label:
-    def __init__(self, label):
-        self.label = label
-
-    # make it hashable so it can be looked up in the dictionary
-    def __hash__(self):
-        return hash(self.label) # Generate a hash based on the label value
-
-    def __eq__(self, other):
-        # Ensure equality is based on the label value
-        if isinstance(other, Label):
-            return self.label == other.label
-        return False
-
-
-# define session
-class Session:
-    def __init__(self, kind: str):
-        self.kind = kind
-
-class Single(Session):
-    def __init__(self, dir: Dir, payload:str, cont: Session):
-        super().__init__("single")
-        self.dir = dir
-        self.payload = payload
-        self.cont = cont
-
-class Choice(Session):
-    def __init__(self, dir: Dir, alternatives: Dict[Label, Session]):
-        super().__init__("choice")
-        self.dir = dir
-        self.alternatives = alternatives
-    
-    def add(self, name: Label, new_ses: Session):
-        '''
-        Adds a new session to the choice session dictionary.
-
-        Args:
-            name (Label): what the session is called
-            new_ses (Session): actual session to be added
-        '''
-        if name in self.alternatives:
-            print(f"Error: defining existing session {name.label}") # not handled as exception but could be
-        else:
-            self.alternatives[name] = new_ses
-    
-    def lookup(self, name: Label) -> Optional[Session]:
-        '''
-        Looks for session in alternatives (the choice session dictionary)
-
-        Args:
-            name (Label): name of session
         
-        Returns:
-            A session if there is one in the dictionary and nothing otherwise
-        '''
-        if not name in self.alternatives:
-            print(f"The session {name.label} does not exist.") # not handled as exception but could be
-        else:
-            return self.alternatives[name]
-
-class Def(Session):
-    def __init__(self, name: str, cont: Session):
-        super().__init__("def")
-        self.name = name
-        self.cont = cont
-
-class Ref(Session):
-    def __init__(self, name: str):
-        super().__init__("ref")
-        self.name = name
-
-class End(Session):
-    def __init__(self):
-        super().__init__("end")
-
-# define schema validation fail error
-class SchemaValidationError(Exception):
-    """Exception raised for errors in schema validation."""
-    def __init__(self, message="Schema validation failed"):
-        self.message = message
-        super().__init__(self.message)
-
 # ---- Client and server communications, session handlers -----------------------------------------------
 
-# global dictionary to keep info about protocols and sessions
-class GlobalDict:
-    def __init__(self, records: Dict[Label, Session]):
-        self.dir = dir
-        self.records = records
-    
-    def add(self, def_ses=Def):
-        '''
-        Adds a new protocol to the global dicitonary of protocols.
-
-            Args:
-                def_ses (Def): def session that works as protocol
-        '''
-        if def_ses.name in self.records:
-            print(f"The session {def_ses.name} already exists. Please define a new one.") # not handled as exception but could be
-        else:
-            print(f"Defining protocol {def_ses.name}...") # to track what proxy is doing at moment -> could be removed
-            self.records[def_ses.name] = def_ses.cont
-    
-    def lookup(self, name: Label) -> Optional[Session]:
-        '''
-        Function that returns a protocol's session, usually in the form of a Choice session
-
-            Args:
-                name (Label): name of the protocol
-
-            Returns:
-                A protocol's session if found in the global dictionary or nothing otherwise
-        '''
-        # print(f"Looking for label: {name} in dictionary...") # comment out to debug
-        if not name in self.records:
-            print(f"The session {name.label} does not exist.") # not handled as exception but could be
-        else:
-            return self.records[name]
-
-
 async def handle_session(ses_server: Session, ses_client: Session, server_socket, client_socket, 
-                         server_parser: Callable=None, client_parser: Callable=None, command:str="") -> tuple[Session, Session] | Exception:
+                         server_parser: Callable, client_parser: Callable, command:str="") -> tuple[Session, Session] | Exception:
     '''
     Performs actions depending on the given sessions and compares the server and client sessions are actually mirrored.
     Def sessions are not handled here because those define protocols and are instead handled in the define_protocols function.
